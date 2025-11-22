75af4d3f4116456ffd0ca18f8a9dbb7902e5ac31
reav <reav1985@gmail.com>
2025-11-22 15:16:51 +0100

    index.html

---
+++ public/service-worker.js
@@ -0,0 +1,44 @@
+// service-worker.js
+const CACHE_NAME = 'videocall-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/images/logo.png',
+  // Aggiungi qui eventuali altre immagini o icone che usi
+];
+
+// 1. Installazione: Caching dei file statici
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Attivazione: Pulizia vecchie cache (se cambi versione)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Fetch: Serve i file dalla cache se offline, altrimenti dalla rete
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
